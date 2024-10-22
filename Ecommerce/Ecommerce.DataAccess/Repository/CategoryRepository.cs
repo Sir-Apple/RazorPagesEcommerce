16842de11eda13c0778ed61aa4ef62f43b2b8b3f
Dovalkin <tranquangtuananh2801@gmail.com>
2024-10-23 01:50:19 +1000

    Create CategoryRepository.cs

---
+++ Ecommerce/Ecommerce.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+	public class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}      
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
+
+		public void Update(Category category)
+		{
+			var objFromDb = _db.Category.FirstOrDefault(u=> u.Id == category.Id);
+			objFromDb.Name = category.Name;
+			objFromDb.DisplayOrder = category.DisplayOrder;
+
+		}
+	}
+}
